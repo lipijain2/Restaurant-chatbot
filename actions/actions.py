4994b74c2031caf7faae2f9d79d4852c734e42cb
Lipi Jain <lipijain@iitk.ac.in>
2021-05-17 01:24:33 +0530

    managed time entity using custom action form

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,57 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+from typing import Text, List, Any, Dict, Optional
+
+from rasa_sdk import Tracker, FormValidationAction
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+from datetime import datetime as dt
+import dateutil.parser
+
+
+
+class ValidateRestaurantForm(FormValidationAction):
+  def name(self) -> Text:
+    return "validate_restaurant_form"
+
+  async def required_slots(
+        self,
+        slots_mapped_in_domain: List[Text],
+        dispatcher: "CollectingDispatcher",
+        tracker: "Tracker",
+        domain: "DomainDict",
+    ) -> Optional[List[Text]]:
+    required_slots = ["time"] + slots_mapped_in_domain
+    return required_slots
+    
+  async def extract_time(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+  ) -> Dict[Text, Any]:
+    datetime = tracker.get_slot("time")
+    if (datetime == None):
+      return {}
+    datetime_obj = dateutil.parser.parse(datetime)
+    humanDate = datetime_obj.strftime('%H:%M')
+    return {"time": humanDate}
+
+  async def validate_time(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+    datetime = dateutil.parser.parse(slot_value)
+    time = datetime.strftime('%H:%M')
+    hours = datetime.strftime('%H')
+    print(time)
+    if (int(hours) < 19 or int(hours) > 22):
+      print(hours)
+      dispatcher.utter_message(text="Please enter a valid time. :(")
+      return {"time": None}
+    return {"time": time}
